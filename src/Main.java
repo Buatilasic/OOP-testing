7937e520b8f273718f3db395cb3bc176a663265a
New <devavovav@gmail.com>
2025-08-07 03:31:44 +0600

    feat: change storage logic to use hashmaps

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,44 @@
-import java.util.Scanner;
-
-import java.util.ArrayList;
-
+import java.util.HashMap;
 
 class User{
-
     private int id;
-
     private String username;
-
     public User(int id, String username) {
-
         this.id = id;
-
         this.username = username;
-
     }
 
     public int getId() {
-
         return id;
-
     }
 
     public String getUsername() {
-
         return username;
-
     }
 
 }
 
 
 class Question {
-
     private int id;
-
     private String questionText;
-
     private String answerText;
-
     public Question(int id, String questionText, String answerText) {
-
         this.id = id;
-
         this.answerText = answerText;
-
         this.questionText = questionText;
-
     }
 
     public int getId(){
-
         return id;
-
     }
 
     public String getQuestionText(){
-
         return questionText;
-
     }
 
     public String getAnswerText(){
-
         return answerText;
-
     }
 
 }
@@ -74,23 +47,24 @@ class Question {
 public class Main {
 
     public static void main(String[] args) {
-
+        HashMap<Integer, User> userMap = new HashMap<>();
+        HashMap<Integer, Question> questionMap = new HashMap<>();
         User user1 = new User(0, "Buatilasic");
-
         User user2 = new User(1, "Aiva");
-
-        System.out.println("Пользователь " + user1.getUsername() + " имеет id " + user1.getId());
-
-        System.out.println("Пользователь " + user2.getUsername() + " имеет id " + user2.getId());
-
+        userMap.put(user1.getId(), user1);
+        userMap.put(user2.getId(), user2);
+        for (int i : userMap.keySet()) {
+            User retrievedUser = userMap.get(i);
+            System.out.println("У пользователя с id" + i + " загружено имя " + retrievedUser.getUsername());
+        }
         Question question1 = new Question(0, "Какой сейчас год?", "2025");
-
         Question question2 = new Question(1, "Сколько пальцев на руке у среднестатистического человека?", "5");
-
-        System.out.println("Вопрос №" + question1.getId() + ": " + question1.getQuestionText() + " Ответ: " + question1.getAnswerText());
-
-        System.out.println("Вопрос №" + question2.getId() + ": " + question2.getQuestionText() + " Ответ: " + question2.getAnswerText());
-
+        questionMap.put(question1.getId(), question1);
+        questionMap.put(question2.getId(), question2);
+        for (int i : questionMap.keySet()) {
+            Question retrievedQuestion = questionMap.get(i);
+            System.out.println("Вопрос id" + i + ": " + retrievedQuestion.getQuestionText() + " Ответ: " + retrievedQuestion.getAnswerText());
+        }
     }
 
 }
