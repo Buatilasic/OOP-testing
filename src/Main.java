a75550cfcd7c21e2acce3dd72c7db76f4e3f8e08
New <devavovav@gmail.com>
2025-08-07 03:47:10 +0600

    feat: add question list to User class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 import java.util.HashMap;
+import  java.util.ArrayList;
+import java.util.List;
 
 class User{
     private int id;
     private String username;
+    private List<Question> questions;
     public User(int id, String username) {
         this.id = id;
         this.username = username;
+        this.questions = new ArrayList<>();
     }
 
     public int getId() {
@@ -16,6 +20,13 @@ class User{
         return username;
     }
 
+    public void addQuestion(Question question) {
+        this.questions.add(question);
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
 }
 
 
@@ -53,14 +64,20 @@ public class Main {
         User user2 = new User(1, "Aiva");
         userMap.put(user1.getId(), user1);
         userMap.put(user2.getId(), user2);
+        Question question1 = new Question(0, "Какой сейчас год?", "2025");
+        Question question2 = new Question(1, "Сколько пальцев на руке у среднестатистического человека?", "5");
+        user1.addQuestion(question1);
+        user2.addQuestion(question2);
+        questionMap.put(question1.getId(), question1);
+        questionMap.put(question2.getId(), question2);
         for (int i : userMap.keySet()) {
             User retrievedUser = userMap.get(i);
             System.out.println("У пользователя с id" + i + " загружено имя " + retrievedUser.getUsername());
+            System.out.println("Список вопросов пользователя:");
+            for (Question q : retrievedUser.getQuestions()) {
+                System.out.println("- " + q.getQuestionText());
+            }
         }
-        Question question1 = new Question(0, "Какой сейчас год?", "2025");
-        Question question2 = new Question(1, "Сколько пальцев на руке у среднестатистического человека?", "5");
-        questionMap.put(question1.getId(), question1);
-        questionMap.put(question2.getId(), question2);
         for (int i : questionMap.keySet()) {
             Question retrievedQuestion = questionMap.get(i);
             System.out.println("Вопрос id" + i + ": " + retrievedQuestion.getQuestionText() + " Ответ: " + retrievedQuestion.getAnswerText());
